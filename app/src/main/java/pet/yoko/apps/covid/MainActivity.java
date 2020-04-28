a4496aed5b775238542210855c4ea09a8b0ef52d
Rafael Perzzzo <rafaelperazzo@gmail.com>
2020-04-28 13:42:55 -0300

    Primeiro commit

---
+++ app/src/main/java/pet/yoko/apps/covid/MainActivity.java
@@ -0,0 +1,94 @@
+package pet.yoko.apps.covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    public String url = "https://apps.yoko.pet/api/covidapi.php?resumo=";
+    TextView confirmados;
+    TextView suspeitos;
+    TextView obitos;
+    TextView taxa;
+    TextView atualizacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        atualizacao = (TextView)findViewById(R.id.atualizacao);
+        confirmados = (TextView)findViewById(R.id.confirmados);
+        suspeitos = (TextView)findViewById(R.id.suspeitos);
+        obitos = (TextView)findViewById(R.id.obitos);
+        taxa = (TextView)findViewById(R.id.taxa);
+
+        try {
+            run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void btnMapaCidadesClick(View view) {
+
+    }
+
+    void run() throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject obj = new JSONObject(myResponse);
+                            confirmados.setText(obj.getString("confirmados"));
+                            suspeitos.setText(obj.getString("suspeitos"));
+                            obitos.setText(obj.getString("obitos"));
+                            double dblTaxa = obj.getDouble("taxa");
+                            String strTaxa = String.format("%.2f",dblTaxa);
+                            taxa.setText(strTaxa);
+                            String data = obj.getString("atualizacao");
+                            atualizacao.setText(data);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+}
